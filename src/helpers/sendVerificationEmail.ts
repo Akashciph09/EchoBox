fde3fff5a06e8d3ddba1304da7c28f18c954cda1
Akash Singh <akashsingh512367@gmail.com>
2025-09-27 03:47:25 +0530

    added zod for validation,database connection ,resend email ,user signup and custom otp fetures

---
+++ src/helpers/sendVerificationEmail.ts
@@ -0,0 +1,23 @@
+import { resend } from "@/lib/resend";
+import VerificationEmail from "../../emails/VerificationEmail";
+
+import { ApiResponse } from "@/types/ApiResponse";
+
+export async function sendverificationEmail(
+  email: string,
+  username: string,
+  verifyCode: string
+): Promise<ApiResponse> {
+  try {
+    await resend.emails.send({
+      from: "you@example.com",
+      to: email,
+      subject: "EchoBox | verification Code",
+      react: VerificationEmail({ username, otp: verifyCode }),
+    });
+    return { success: true, message: "Verification email send successfully" };
+  } catch (emailError) {
+    console.log("Error sedning email verification email", emailError);
+    return { success: false, message: "Failed to send verification email" };
+  }
+}
